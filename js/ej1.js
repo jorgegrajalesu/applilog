273ec03661cb47bb13e0aa8df34fce1352788c46
jorgegrajalesu <jegosystem@gmail.com>
2025-08-07 21:21:10 -0500

    070825

---
+++ js/ej1.js
@@ -0,0 +1,10 @@
+let x;
+let y;
+let suma;
+x=7;
+y=2;
+suma = x + y;
+
+document.getElementById("x").innerHTML=x;
+document.getElementById("y").innerHTML=y;
+document.getElementById("suma").innerHTML = suma;
