5d2cb8321ca123660bf4ba36d2622d22208bd770
jorgegrajalesu <jegosystem@gmail.com>
2025-08-09 16:34:11 -0500

    090825

---
+++ js/variables.js
@@ -0,0 +1,11 @@
+//1 opcion aunque ya no se utiliza tanto
+var num1 = 5;
+//2 opcion es con let
+let num2 = 5;
+let suma = num1 + num2;
+let cadena='hola jorge';
+let resul = suma + num2;
+alert(suma);
+alert(resul);
+console.log(suma);
+console.log(resul);
